238da612cffdd9afa062ed30ec8bbe72b8d320b2
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-07-23 14:03:05 +0600

    Result

---
+++ src/Result.java
@@ -0,0 +1,16 @@
+public class Result {
+public static void main(String[]args) {
+
+    int Marks=75;
+    if (Marks>=80) {System.out.println("A+");}
+
+
+
+
+     else if (Marks>=60) { System.out.println("A");}   
+      
+      
+      else {System.out.println("Fail"); }
+
+ }
+}
